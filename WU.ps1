fa11e03b24c7ec7f2d497dffa142d7efbb845070
chris-bosman <christopher.r.bosman@gmail.com>
2018-03-26 09:05:49 -0500

    Added shutdown command

--- WU.ps1
+++ WU.ps1
@@ -1,4 +1,3 @@
-### Check for and install Get-WUInstall Prerequisites
 # Download PSWindowsUpdate
 $Destination = "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
 Invoke-WebRequest -Uri "https://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/47/PSWindowsUpdate.zip" -OutFile "$Destination\PSWindowsUpdate.zip"
@@ -15,7 +14,7 @@ ForEach ($Item in $Zip.Items())
 # Import Module
 Import-Module PSWindowsUpdate
 
-### Add Windows Update Service Manager ID
+# Add Windows Update Service Manager ID
 If ((Get-WUServiceManager | Select-Object ServiceID) -match "7971f918-a847-4430-9279-4a52d1efe18d")
     {
         Write-Host "Required Service Manager imported, continuing..."
@@ -25,5 +24,8 @@ else
         Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
     }
 
-### Install Updates But Supress Reboot
+# Install Updates
 Get-WUInstall -MicrosoftUpdate -AcceptAll -AutoReboot -Confirm:$false
+
+# Reboot
+cmd /c shutdown /r /t 0
